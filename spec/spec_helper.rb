66feb552c25c95a742f00e4c9810ac38f2a59fc2
Valentin Vasilyev <iamvalentin@gmail.com>
2013-03-25 12:27:25 +0400

    Coveralls config change

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
-require 'bundler/setup'
+require 'coveralls'
+Coveralls.wear!require 'bundler/setup'
+
 require 'active_record'
 require_relative '../lib/vostok/rails'
 
-require 'coveralls'
-Coveralls.wear!
